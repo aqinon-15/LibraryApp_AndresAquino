1fd01c3561590bf34390fec0255976da588307a9
aqinon-15 <aaquino-2023534@kinal.edu.gt>
2026-09-18 02:41:08 -0600

    se subio Venta

---
+++ LibraryApp_AndresAquino/src/org/ae/model/Venta.java
@@ -0,0 +1,60 @@
+package org.ae.model;
+
+public class Venta {
+    private int noVenta;
+    private String fechaVenta;
+    private double totalVenta;
+    private long cuiCliente;
+    private int idUsuario;
+
+    public Venta() {
+    }
+
+    public Venta(int noVenta, String fechaVenta, double totalVenta, long cuiCliente, int idUsuario) {
+        this.noVenta = noVenta;
+        this.fechaVenta = fechaVenta;
+        this.totalVenta = totalVenta;
+        this.cuiCliente = cuiCliente;
+        this.idUsuario = idUsuario;
+    }
+
+    public int getNoVenta() {
+        return noVenta;
+    }
+
+    public void setNoVenta(int noVenta) {
+        this.noVenta = noVenta;
+    }
+
+    public String getFechaVenta() {
+        return fechaVenta;
+    }
+
+    public void setFechaVenta(String fechaVenta) {
+        this.fechaVenta = fechaVenta;
+    }
+
+    public double getTotalVenta() {
+        return totalVenta;
+    }
+
+    public void setTotalVenta(double totalVenta) {
+        this.totalVenta = totalVenta;
+    }
+
+    public long getCuiCliente() {
+        return cuiCliente;
+    }
+
+    public void setCuiCliente(long cuiCliente) {
+        this.cuiCliente = cuiCliente;
+    }
+
+    public int getIdUsuario() {
+        return idUsuario;
+    }
+
+    public void setIdUsuario(int idUsuario) {
+        this.idUsuario = idUsuario;
+    }
+}
