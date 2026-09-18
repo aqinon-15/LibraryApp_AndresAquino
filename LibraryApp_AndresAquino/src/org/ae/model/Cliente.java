a0fc6ad4dfbecb616eaf4f10b88606e966e1fe2f
aqinon-15 <aaquino-2023534@kinal.edu.gt>
2026-09-17 23:52:43 -0600

    se subio cliente

---
+++ LibraryApp_AndresAquino/src/org/ae/model/Cliente.java
@@ -0,0 +1,56 @@
+package org.ae.model;
+
+public class Cliente {
+    private long cui;
+    private String nombreCliente;
+    private String apellidoCliente;
+    private String correoElectronico;
+    
+    public Cliente() {
+    }
+    
+   public Cliente(long cui, String nombreCliente, String apellidoCliente, String correoElectronico) {
+        this.cui = cui;
+        this.nombreCliente = nombreCliente;
+        this.apellidoCliente = apellidoCliente;
+        this.correoElectronico = correoElectronico;
+    }
+
+    public long getCui() {
+        return cui;
+    }
+
+    public void setCui(long cui) {
+        this.cui = cui;
+    }
+
+    public String getNombreCliente() {
+        return nombreCliente;
+    }
+
+    public void setNombreCliente(String nombreCliente) {
+        this.nombreCliente = nombreCliente;
+    }
+
+    public String getApellidoCliente() {
+        return apellidoCliente;
+    }
+
+    public void setApellidoCliente(String apellidoCliente) {
+        this.apellidoCliente = apellidoCliente;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        this.correoElectronico = correoElectronico;
+    }
+
+    @Override
+    public String toString() {
+        return nombreCliente + " " + apellidoCliente;
+    }
+}
+
