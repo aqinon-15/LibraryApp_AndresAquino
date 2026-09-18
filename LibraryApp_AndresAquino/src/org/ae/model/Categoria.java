6c007edc4bc6a45d9dae11b1171510d2b96b3a3d
aqinon-15 <aaquino-2023534@kinal.edu.gt>
2026-09-17 23:16:44 -0600

    se hizo Categorias

---
+++ LibraryApp_AndresAquino/src/org/ae/model/Categoria.java
@@ -0,0 +1,34 @@
+package org.ae.model;
+    
+    public class Categoria {
+        private int idCategoria;
+        private String nombreCategoria;
+        
+        public Categoria (){
+        }
+        
+        public Categoria(int idcategoria, String nombreCategoria){
+            this.idCategoria = idCategoria;
+            this.nombreCategoria = nombreCategoria;
+        }
+        
+        public int getIdCategoria (){
+            return idCategoria;
+        }
+        
+        public void setIdCategoria(int idCategoria) {
+            this.idCategoria = idCategoria;
+        }
+        public String getNombreCategoria() {
+            return nombreCategoria;
+        }
+        
+        public void setNombreCategoria(String nombreCategoria) {
+            this.nombreCategoria = nombreCategoria;
+        }
+        
+        @Override
+        public String toString() {
+            return nombreCategoria;
+        }
+    }   
