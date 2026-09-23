1c5cbe8d11db331f054f42e57aa626fbaa356fc4
Andres Aquino <aaquino-2023534@kinal.edu.gt>
2026-09-23 11:47:28 -0600

    se subio Lineafactura con java doc a medias, terminar luego

--- LibraryApp_AndresAquino/src/org/ae/model/LineaFactura.java
+++ LibraryApp_AndresAquino/src/org/ae/model/LineaFactura.java
@@ -1,5 +1,10 @@
 package org.ae.model;
 
+/**
+ *
+ * @author Andres Aquino
+ * @version 1.0
+ */
 public class LineaFactura {
     private int numeroFactura;
     private String fechaEmision;
@@ -14,101 +19,202 @@ public class LineaFactura {
     private String usuarioAtendio;
     private double granTotal;
 
+    /**
+     *constructor por defecto sin parametros
+     */
     public LineaFactura() {
     }
 
+    /**
+     *obtiene e numero identificador de la factura
+     * 
+     * 
+     * @return el numero de la factura
+     */
     public int getNumeroFactura() {
         return numeroFactura;
     }
 
+    /**
+     *
+     * @param numeroFactura
+     */
     public void setNumeroFactura(int numeroFactura) {
         this.numeroFactura = numeroFactura;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getFechaEmision() {
         return fechaEmision;
     }
 
+    /**
+     *
+     * @param fechaEmision
+     */
     public void setFechaEmision(String fechaEmision) {
         this.fechaEmision = fechaEmision;
     }
 
+    /**
+     *
+     * @return
+     */
     public long getCuiCliente() {
         return cuiCliente;
     }
 
+    /**
+     *
+     * @param cuiCliente
+     */
     public void setCuiCliente(long cuiCliente) {
         this.cuiCliente = cuiCliente;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getNombreCliente() {
         return nombreCliente;
     }
 
+    /**
+     *
+     * @param nombreCliente
+     */
     public void setNombreCliente(String nombreCliente) {
         this.nombreCliente = nombreCliente;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getCorreoCliente() {
         return correoCliente;
     }
 
+    /**
+     *
+     * @param correoCliente
+     */
     public void setCorreoCliente(String correoCliente) {
         this.correoCliente = correoCliente;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getIsbnLibro() {
         return isbnLibro;
     }
 
+    /**
+     *
+     * @param isbnLibro
+     */
     public void setIsbnLibro(String isbnLibro) {
         this.isbnLibro = isbnLibro;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getTituloLibro() {
         return tituloLibro;
     }
 
+    /**
+     *
+     * @param tituloLibro
+     */
     public void setTituloLibro(String tituloLibro) {
         this.tituloLibro = tituloLibro;
     }
 
+    /**
+     *
+     * @return
+     */
     public int getCantidad() {
         return cantidad;
     }
 
+    /**
+     *
+     * @param cantidad
+     */
     public void setCantidad(int cantidad) {
         this.cantidad = cantidad;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getPrecioUnitario() {
         return precioUnitario;
     }
 
+    /**
+     *
+     * @param precioUnitario
+     */
     public void setPrecioUnitario(double precioUnitario) {
         this.precioUnitario = precioUnitario;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getSubtotal() {
         return subtotal;
     }
 
+    /**
+     *
+     * @param subtotal
+     */
     public void setSubtotal(double subtotal) {
         this.subtotal = subtotal;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getUsuarioAtendio() {
         return usuarioAtendio;
     }
 
+    /**
+     *
+     * @param usuarioAtendio
+     */
     public void setUsuarioAtendio(String usuarioAtendio) {
         this.usuarioAtendio = usuarioAtendio;
     }
 
+    /**
+     *
+     * @return
+     */
     public double getGranTotal() {
         return granTotal;
     }
 
+    /**
+     *
+     * @param granTotal
+     */
     public void setGranTotal(double granTotal) {
         this.granTotal = granTotal;
     }
