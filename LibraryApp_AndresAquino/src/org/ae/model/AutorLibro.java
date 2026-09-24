25027e190aae37fb369adb77a6eb4b145c80b55a
Andres Aquino <aaquino-2023534@kinal.edu.gt>
2026-09-24 10:26:53 -0600

    se subio AutorLibro con JAVADOC y le faltaba una llave por eso daba error

--- LibraryApp_AndresAquino/src/org/ae/model/AutorLibro.java
+++ LibraryApp_AndresAquino/src/org/ae/model/AutorLibro.java
@@ -1,24 +1,46 @@
 package org.ae.model;
 
+/**
+ *
+ * @author Andres Aquino
+ */
 public class AutorLibro {
     private int idAutorLibro;
     private int idAutor;
     private String isbn;
     
+    /**
+     *
+     */
     public AutorLibro() {
     }
     
+    /**
+     *
+     * @param idAutorLibro damos los id o usuario de AutorLibro
+     * @param idAutor damos los usuarios de autor
+     * @param isbn damos los isbn osea sistema universal de enumeracion
+     */
     public AutorLibro(int idAutorLibro, int idAutor, String isbn) {
         this.idAutorLibro = idAutorLibro;
                 this.idAutor = idAutor;
                 this.isbn = isbn;
     }
     
-public int getIdAutorLibro() {
+    /**
+     *
+     * @return pedimos los autor libro de get 
+     */
+    public int getIdAutorLibro() {
     return idAutorLibro;
 }
 
-public void setIdAutorLibro(int idAutorLibro) {
+    /**
+     *
+     * @param idAutorLibro damos los AutorLibro de set 
+     */
+    public void setIdAutorLibro(int idAutorLibro) {
     this.idAutorLibro = idAutorLibro;
 }
 
+}
